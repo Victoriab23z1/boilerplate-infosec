cd037fa00126a08161aee57bfe295b00f1d33ff4
Victoriab23z1 <165341608+Victoriab23z1@users.noreply.github.com>
2025-07-13 16:06:49 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,60 +1,22 @@
-const helmet = require("helmet");
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
 
+// ✅ Ocultar el header X-Powered-By
+app.use(helmet.hidePoweredBy());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+
+// ✅ Exportar la app SOLO aquí
+module.exports = app;
